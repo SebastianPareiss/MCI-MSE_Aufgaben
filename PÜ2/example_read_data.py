cbaec77eb075aadba2cea318635e9b8014d67ec3
SebastianPareiss <101809039+SebastianPareiss@users.noreply.github.com>
2022-03-21 13:33:37 +0100

    Update example_read_data.py

--- PÜ2/example_read_data.py
+++ PÜ2/example_read_data.py
@@ -2,6 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Schleife um jede Person einspeichern zu können
+
 # %% Öffnen der Datei und konvertieren zu numpy-Array
 file_name =  'input_data/power_data_1.txt'
 power_data_watts = open(file_name).read().split("\n")
