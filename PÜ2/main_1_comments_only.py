b3a9c5791f36149641290efa1b7883bb74cbd5d9
SebastianPareiss <ps2030@mci4me.at>
2022-03-28 19:42:56 +0200

    Upload Kommentare PÜ_2

--- PÜ2/main_1_comments_only.py
+++ PÜ2/main_1_comments_only.py
@@ -1,4 +1,30 @@
-# UC 2.0
+#UC 2.0 Auswertung der Leistungsdaten
 
 #%% UC 2.1 Einlesen der Daten
+# Daten welche auf Ergometer erfasst wurden müssen eingelesen werden
+# Eingelesene Daten müssen zur weiteren Verarbeitung auf einem Zwischenspeicher "abgelagert" werden
 
+#%% UC 2.2 Vorverarbeitung der Daten
+# Daten müssen in ein entsprechendes Format gebracht werden, damit diese verarbeitbar sind 
+# Daten müssen entsprechend sortiert und Ergebnisse intepretiert (laut Anforderung) werden
+# Ohne Vorverabreitung wären Daten nicht für die Analyse bereit 
+
+#%%UC 2.3 Analysieren der Daten auf Abbruchkriterium
+# Entsprechend gegebenen Anforderungen und Abbruchkriterien werden Daten sortiert 
+# Bei "erreichen" bestimmter Werte endet die Analyse 
+
+#%%UC 2.4 Ertsellen einer Zusammenfassung
+# Verarbeitete Werte müssen für jeden Probanden geordnet werden
+# Jeder Proband erhält spezifische Zusammenfassung der Werte
+
+#%%UC 2.5 Visualisierung der Daten
+# Analysierte Daten werden in einem entsprechendem Plot ausgegeben
+# Für jeden Probanden wird ein eigener Plot erstellt 
+# Visualisierung kann je nach Anforderung angepasst werden
+#%%UC 2.6 Manuelle Eingabe eines Abbruch-Kriteriums 
+# In Sonderfällen kann manuelle Eingabe gewünscht sein 
+# Abbrechen der Analyse kann individuell angepasst werden
+
+#%%UC 2.7 Speichern der Daten
+# Fertige Analyse und Visualisierungen werden auf einem bestimmten Endspeicher gespeichert 
+# Auf diesem Speicher besteht immer Zugriff und die Ergebnisse können eingesehen werden
